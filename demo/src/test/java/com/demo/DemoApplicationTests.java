17a04ae93e638e243026a2fb6c202a23589590c5
asheesh859 <ak2305@checkboxtechnology.com>
2025-06-14 18:15:48 +0530

    Junit Test case add 14/06/2025 6:15 PM

---
+++ demo/src/test/java/com/demo/DemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLOutput;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Strat");
+	}
+	@BeforeAll
+    static void test1(){
+		System.out.println("Before all method in test case");
+	}
+	@AfterAll
+    static void test2(){
+		System.out.println("After all test case ");
+	}
+	@AfterEach
+	void test3(){
+
+	}
+	@BeforeEach
+	void test4(){
+		System.out.println("Before each method");
+	}
+
+}
