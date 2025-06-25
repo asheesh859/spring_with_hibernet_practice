f05a047aa6e0dea248d25bdbd4b4ea87b5b1b52c
asheesh859 <ak2305@checkboxtechnology.com>
2025-06-25 14:55:47 +0530

    Add trasection anotation and bidirectional mapping 25/06/2025 2:55 PM

--- demo/src/main/java/com/demo/entity/Post_1.java
+++ demo/src/main/java/com/demo/entity/Post_1.java
@@ -1,50 +1,50 @@
-package com.demo.entity;
+    package com.demo.entity;
 
-import jakarta.persistence.*;
+    import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-@Entity
-@Table(name = "Post_1")
-public class Post_1 {
+    @Entity
+    @Table(name = "Post_1")
+    public class Post_1 {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    private String title;
-
-
-//    @OneToMany(mappedBy = "Post_1", cascade = CascadeType.ALL)
-//    private List<Comments_1> comments_1 = new ArrayList<>();
-
-    @OneToMany(cascade = CascadeType.ALL ,fetch = FetchType.EAGER)
-    @JoinColumn(name = "post_id")
-    private List<Comments_1> comments_1 = new ArrayList<>();
+        private String title;
 
 
-    public Long getId() {
-        return id;
+        @OneToMany(mappedBy = "post_1", cascade = CascadeType.ALL)
+        private List<Comments_1> comments_1 = new ArrayList<>();
+
+    //    @OneToMany(cascade = CascadeType.ALL ,fetch = FetchType.EAGER)
+    //    @JoinColumn(name = "post_id")
+    //    private List<Comments_1> comments_1 = new ArrayList<>();
+
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<Comments_1> getComments_1() {
+            return comments_1;
+        }
+
+        public void setComments_1(List<Comments_1> comments_1) {
+            this.comments_1 = comments_1;
+        }
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public List<Comments_1> getComments_1() {
-        return comments_1;
-    }
-
-    public void setComments_1(List<Comments_1> comments_1) {
-        this.comments_1 = comments_1;
-    }
-}
--- demo/src/test/java/com/demo/bloggingTests.java
+++ demo/src/test/java/com/demo/bloggingTests.java
@@ -3,12 +3,14 @@ package com.demo;
 import com.demo.entity.Comments_1;
 import com.demo.entity.Post_1;
 import com.demo.repository.*;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class bloggingTests {
@@ -41,6 +43,18 @@ class bloggingTests {
 	@Test
 	void deletePost(){
 		post1Repository.deleteById(1L);
+	}
+
+	@Transactional
+	@Test
+	void getPostAndItsComments(){
+	Post_1 post1 = post1Repository.findById(1L).orElseThrow();
+	List<Comments_1> comments1List = post1.getComments_1();
+	for(Comments_1 result : comments1List){
+		System.out.println(result.getId());
+		System.out.println(result.getContent());
+	}
+
 	}
 
 }
