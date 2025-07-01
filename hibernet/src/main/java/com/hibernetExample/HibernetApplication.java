231558238094dcd45688d6e45df3abe7ebf886c9
asheesh859 <ak2305@checkboxtechnology.com>
2025-07-01 15:01:29 +0530

    modify project

--- hibernet/src/main/java/com/hibernetExample/HibernetApplication.java
+++ hibernet/src/main/java/com/hibernetExample/HibernetApplication.java
@@ -1,6 +1,4 @@
 package com.hibernetExample;
-import com.hibernetExample.utils.HibernetUtils;
-import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,29 +10,6 @@ public abstract class HibernetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernetApplication.class, args);
-
-		Student student = new Student();
-		student.setName("Ashu");
-		student.setActive(true);
-		student.setCollege("GLA University");
-		student.setMobile("9808508760");
-		student.setAbout("software engineer");
-		SessionFactory sessionFactory = HibernetUtils.getSessionFactory();
-
-		Session session = sessionFactory.openSession();
-		Transaction transaction = null;
-		try{
-		transaction = session.beginTransaction();
-		session.persist(student);
-			System.out.println("student saved..");
-		} catch (Exception e) {
-
-			if(transaction!=null){
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-
 
 	}
 
